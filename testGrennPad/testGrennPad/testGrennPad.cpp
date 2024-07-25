1d20f01b8059ddb36d141cd98c90b985b2f29138
turican0 <41465132+turican0@users.noreply.github.com>
2024-07-25 20:37:05 +0200

    fix

--- testGrennPad/testGrennPad/testGrennPad.cpp
+++ testGrennPad/testGrennPad/testGrennPad.cpp
@@ -1,20 +1,153 @@
-﻿// testGrennPad.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
-//
+﻿#include <windows.h>
+#include <string>
 
-#include <iostream>
+// Velikost bufferu pro text
+const int BUFFER_SIZE = 1024;
+
+// Globální proměnné
+HINSTANCE hInst;
+HWND hWnd;
+SCROLLINFO si;
+int yPos = 0;
+TCHAR buffer[BUFFER_SIZE] = TEXT("Toto je testovací text. Můžete sem vložit jakýkoliv dlouhý text, který chcete rolovat.\n"
+    "Další řádek textu pro testování rolování.\n"
+    "Ještě jeden řádek textu pro zobrazení.\n");
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+ATOM MyRegisterClass(HINSTANCE hInstance) {
+    WNDCLASSEX wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = TEXT("ScrollTextClass");
+    wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+    return RegisterClassEx(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow) {
+    hInst = hInstance;
+
+    hWnd = CreateWindow(TEXT("ScrollTextClass"), TEXT("Scrolling Text Example"), WS_OVERLAPPEDWINDOW | WS_VSCROLL,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+    if (!hWnd) {
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    return TRUE;
+}
+
+void SetVerticalScrollBar(HWND hWnd) {
+    si.cbSize = sizeof(si);
+    si.fMask = SIF_RANGE | SIF_PAGE;
+    si.nMin = 0;
+    si.nMax = 100;  // Nastavte podle potřeby
+    si.nPage = 10;  // Nastavte podle potřeby
+    SetScrollInfo(hWnd, SB_VERT, &si, TRUE);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    PAINTSTRUCT ps;
+    HDC hdc;
+    RECT rect;
+    TEXTMETRIC tm;
+    int charHeight;
+
+    switch (message) {
+    case WM_CREATE:
+        SetVerticalScrollBar(hWnd);
+        break;
+
+    case WM_SIZE:
+        SetVerticalScrollBar(hWnd);
+        break;
+
+    case WM_VSCROLL:
+        switch (LOWORD(wParam)) {
+        case SB_LINEUP:
+            yPos -= 1;
+            break;
+        case SB_LINEDOWN:
+            yPos += 1;
+            break;
+        case SB_PAGEUP:
+            yPos -= 10;
+            break;
+        case SB_PAGEDOWN:
+            yPos += 10;
+            break;
+        case SB_THUMBTRACK:
+            yPos = HIWORD(wParam);
+            break;
+        }
+
+        // Ujistěte se, že yPos zůstane v povoleném rozsahu
+        yPos = max(0, min(yPos, 100));
+
+        si.fMask = SIF_POS;
+        si.nPos = yPos;
+        SetScrollInfo(hWnd, SB_VERT, &si, TRUE);
+
+        InvalidateRect(hWnd, NULL, TRUE);
+        break;
+
+    case WM_PAINT:
+        hdc = BeginPaint(hWnd, &ps);
+
+        GetClientRect(hWnd, &rect);
+        GetTextMetrics(hdc, &tm);
+        charHeight = tm.tmHeight + tm.tmExternalLeading;
+
+        // Posunout DC podle yPos
+        SetWindowOrgEx(hdc, 0, yPos * charHeight, NULL);
+
+        // Vykreslit text
+        DrawText(hdc, buffer, -1, &rect, DT_LEFT);
+
+        EndPaint(hWnd, &ps);
+        break;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+int MyWinMain(HINSTANCE hInstance, int nCmdShow) {
+
+    MyRegisterClass(hInstance);
+
+    if (!InitInstance(hInstance, nCmdShow)) {
+        return FALSE;
+    }
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    MyWinMain(GetModuleHandle(NULL), SW_SHOW);
 }
-
-// Spuštění programu: Ctrl+F5 nebo nabídka Ladit > Spustit bez ladění
-// Ladění programu: F5 nebo nabídka Ladit > Spustit ladění
-
-// Tipy pro zahájení práce:
-//   1. K přidání nebo správě souborů použijte okno Průzkumník řešení.
-//   2. Pro připojení ke správě zdrojového kódu použijte okno Team Explorer.
-//   3. K zobrazení výstupu sestavení a dalších zpráv použijte okno Výstup.
-//   4. K zobrazení chyb použijte okno Seznam chyb.
-//   5. Pokud chcete vytvořit nové soubory kódu, přejděte na Projekt > Přidat novou položku. Pokud chcete přidat do projektu existující soubory kódu, přejděte na Projekt > Přidat existující položku.
-//   6. Pokud budete chtít v budoucnu znovu otevřít tento projekt, přejděte na Soubor > Otevřít > Projekt a vyberte příslušný soubor .sln.
